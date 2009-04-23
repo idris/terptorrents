fb5e0e9080dc4d69a016c9aabf07cdeabfbdf2a2
idrism <idrism@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-23 17:58:17 +0000

    add address to Peer

--- trunk/terptorrents/src/terptorrents/models/Peer.java
+++ trunk/terptorrents/src/terptorrents/models/Peer.java
@@ -1,4 +1,16 @@
 package terptorrents.models;
 
+import java.net.InetSocketAddress;
+
 public class Peer {
+	InetSocketAddress address;
+
+	/**
+	 * Set the address of this peer using the hostname (or IP address) and port
+	 * @param hostname - hostname or IP address
+	 * @param port
+	 */
+	public void setAddress(String hostname, int port) {
+		address = new InetSocketAddress(hostname, port);
+	}
 }
