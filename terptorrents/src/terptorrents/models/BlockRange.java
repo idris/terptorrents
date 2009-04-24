a89589c218b02852353c9b962f04b02cabb2f460
ljfh001 <ljfh001@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-24 04:21:01 +0000

    done with pieceManager

---
+++ terptorrents/src/terptorrents/models/BlockRange.java
@@ -0,0 +1,17 @@
+/**
+ * 
+ */
+package terptorrents.models;
+
+/**
+ * @author jonli
+ *
+ */
+public class BlockRange {
+	public int begin;
+	public int length;
+	public BlockRange(int begin, int length){
+		this.begin = begin;
+		this.length = length;
+	}
+}
