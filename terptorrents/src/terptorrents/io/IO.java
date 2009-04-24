bb69336c15a342e964ef025797b410ca84f938ed
sshlyk <sshlyk@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-24 03:02:50 +0000

    getEmptyP

--- terptorrents/src/terptorrents/io/IO.java
+++ terptorrents/src/terptorrents/io/IO.java
@@ -51,8 +51,8 @@ public class IO {
 	
 	private class MyIOBitSet implements IOBitSet {
 
-		public Set<Integer> getEmptyPiecesSet() {
-			return new EmptyPiecesSet();
+		public Iterator<Integer> getEmptyPiecesIterator() {
+			return null;
 		}
 
 		public int getNumEmptyPieces() {
@@ -76,72 +76,5 @@ public class IO {
 		}
 		
 	}
-	
-	private class EmptyPiecesSet implements Set<Integer> {
 
-		public boolean add(Integer arg0) {
-			throw new UnsupportedOperationException("EmptyPicesSet does not accept any chages from outside." +
-					"It should be only modified by IO class. ");
-		}
-
-		public boolean addAll(Collection<? extends Integer> arg0) {
-			throw new UnsupportedOperationException();
-		}
-
-		public void clear() {
-			// TODO Auto-generated method stub
-			
-		}
-
-		public boolean contains(Object arg0) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean containsAll(Collection<?> arg0) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean isEmpty() {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public Iterator<Integer> iterator() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public boolean remove(Object arg0) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean removeAll(Collection<?> arg0) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public boolean retainAll(Collection<?> arg0) {
-			// TODO Auto-generated method stub
-			return false;
-		}
-
-		public int size() {
-			// TODO Auto-generated method stub
-			return 0;
-		}
-
-		public Object[] toArray() {
-			// TODO Auto-generated method stub
-			return null;
-		}
-
-		public <T> T[] toArray(T[] arg0) {
-			// TODO Auto-generated method stub
-			return null;
-		}
-		
-	}
 }
