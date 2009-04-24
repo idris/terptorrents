e7ceed8a4b7734856fd12bf6e3b33bc65f6de2b3
samuelbrin <samuelbrin@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-24 04:28:23 +0000

    Added getter to MetaFile for the sha hashes of all pieces

--- terptorrents/src/metainfo/TorrentParser.java
+++ terptorrents/src/metainfo/TorrentParser.java
@@ -4,6 +4,7 @@ import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
 import java.util.Map;
+import java.security.MessageDigest;
 
 import metainfo.*;
 
@@ -23,12 +24,33 @@ public class TorrentParser {
 		}
 	}
 	
-	public void parseTorrent() throws InvalidBEncodingException,IOException, FileNotFoundException{
+	public void parse() throws InvalidBEncodingException,IOException, FileNotFoundException{
 		BDecoder bdecoder=null;
 		bdecoder=new BDecoder(new FileInputStream(filename));
-		
-		
 		topLevelMap=bdecoder.bdecode().getMap();
+		
+		/* pull simple string fields out of top level bencoded dictionary */
+		String announce = ((BEValue)topLevelMap.get("announce")).getString();
+		String comment = ((BEValue)topLevelMap.get("comment")).getString();
+		String createdBy=((BEValue)topLevelMap.get("createdBy")).getString();
+		Long creationDate=((BEValue)topLevelMap.get("creationDate")).getLong();
+		
+		/* parse out fields from info dictionary */
+		Map infoDictionary = ((BEValue)topLevelMap.get("info")).getMap();
+		boolean multiFileMode=false;
+		if(infoDictionary.get("files")!=null)multiFileMode=true;
+		Long pieceLength = ((BEValue)infoDictionary.get("piece length")).getLong();
+		String piecesString=((BEValue)infoDictionary.get("pieces")).getString();
+		
+		
+		
+	}
+	
+	public Map<String,Long> getMultiFileInfo(Map infoHash){
+		
+		//List<String> infoHash.get("files");
+		
+		return null;
 		
 	}
 	
