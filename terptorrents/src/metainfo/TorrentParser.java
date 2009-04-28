dbcc7fe2d4539638fa8b1ac3f89296d8f5c0af2f
samuelbrin <samuelbrin@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-28 06:03:01 +0000

    done coding single file case

--- terptorrents/src/metainfo/TorrentParser.java
+++ terptorrents/src/metainfo/TorrentParser.java
@@ -3,6 +3,11 @@ package metainfo;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.List;
 import java.util.Map;
 import java.security.MessageDigest;
 
@@ -14,6 +19,7 @@ public class TorrentParser {
 	private FileInputStream stream;
 	private String filename;
 	private Map topLevelMap;
+	private MetaFile torrent; //only instantiated after calling parse()
 	
 	public TorrentParser(String filename){
 		this.filename=filename;
@@ -26,32 +32,83 @@ public class TorrentParser {
 	
 	public void parse() throws InvalidBEncodingException,IOException, FileNotFoundException{
 		BDecoder bdecoder=null;
-		bdecoder=new BDecoder(new FileInputStream(filename));
+		bdecoder=new BDecoder(stream);
 		topLevelMap=bdecoder.bdecode().getMap();
 		
 		/* pull simple string fields out of top level bencoded dictionary */
 		String announce = ((BEValue)topLevelMap.get("announce")).getString();
 		String comment = ((BEValue)topLevelMap.get("comment")).getString();
 		String createdBy=((BEValue)topLevelMap.get("createdBy")).getString();
-		Long creationDate=((BEValue)topLevelMap.get("creationDate")).getLong();
+		Date creationDate=new Date(((BEValue)topLevelMap.get("creationDate")).getLong());
 		
-		/* parse out fields from info dictionary */
+		/* pull simple fields out of info dictionary */
 		Map infoDictionary = ((BEValue)topLevelMap.get("info")).getMap();
-		boolean multiFileMode=false;
-		if(infoDictionary.get("files")!=null)multiFileMode=true;
 		Long pieceLength = ((BEValue)infoDictionary.get("piece length")).getLong();
-		String piecesString=((BEValue)infoDictionary.get("pieces")).getString();
+		List files = ((BEValue)(infoDictionary.get("files"))).getList();
+		String name = ((BEValue)(infoDictionary.get("name"))).getString(); //can be filename or directory name
+		
+		/* get concatenated SHA hash values from info dictionary */
+		byte[] allHashes=((BEValue)(infoDictionary.get("pieces"))).getBytes();
+		Map<Integer,byte[]> pieceHashMap;
+		
+		if(files==null){ // single file case
+			Long lastPieceLength=0L;
+			Long fileLength=((BEValue)(infoDictionary.get("length"))).getLong();
+			Long numPieces=fileLength/pieceLength;
+			lastPieceLength=fileLength % pieceLength;
+			if(lastPieceLength != 0) numPieces++; //account for irregular last piece
+			pieceHashMap=getPieceHashes(allHashes, numPieces);
+			List<String>singleFileList=new ArrayList<String>();
+			singleFileList.add(name);
+			Map<String,Long>singleFileLengthMap=new LinkedHashMap<String,Long>();
+			singleFileLengthMap.put(name, fileLength);
+			torrent = new MetaFile(announce, creationDate, comment,
+					createdBy, pieceLength, singleFileList,
+					singleFileLengthMap,pieceHashMap);
+		}
 		
 		
+		//TODO: multi-file case
+		else{}
 		
 	}
 	
-	public Map<String,Long> getMultiFileInfo(Map infoHash){
+	//TODO: multi-file case
+	private Map<String,Long> getMultiFileInfo(Map infoHash){
 		
 		//List<String> infoHash.get("files");
 		
 		return null;
 		
+	}
+	
+	//TODO: multi-file case
+	public Map<String,Long> processFileList(List fileList) throws InvalidBEncodingException{
+		LinkedHashMap<String,Long> returnValue=new LinkedHashMap<String,Long>();
+		for(Object fileObject : fileList){
+			Map fileMap = ((BEValue)fileObject).getMap();
+			List<String> path=((BEValue)fileMap.get("path")).getList();
+			Long length=((BEValue)fileMap.get("length")).getLong();
+		}
+		return null;
+	}
+	
+	/* 
+	 * Takes a byte array consisting of concatenation of all SHA-hashes and separates it into
+	 * 20 byte chunks, mapping each piece number to a 20 byte hash.
+	 */
+	private Map<Integer,byte[]> getPieceHashes(byte[] allHashes, Long numPieces){
+		Map<Integer,byte[]>returnValue=new HashMap<Integer,byte[]>();
+		for(int i=0; i<numPieces; i++){
+			byte[] currentHash=new byte[20];
+			for(int j=0; j<20; j++) currentHash[j]=allHashes[20*i+j];
+			returnValue.put(i,currentHash);
+		}
+		return returnValue;
+	}
+	
+	public MetaFile getMetaFile(){
+		return torrent;
 	}
 	
 	
