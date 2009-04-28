e84bca064dc90832be571c83d370c51a54d8d3cd
idrism <idrism@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-28 19:26:04 +0000

    BitSetUtil

---
+++ trunk/terptorrents/src/terptorrents/util/BitSetUtil.java
@@ -0,0 +1,25 @@
+package terptorrents.util;
+
+import java.util.BitSet;
+
+public final class BitSetUtil {
+	public static BitSet fromByteArray(byte[] bytes) {
+		BitSet bits = new BitSet();
+		for (int i=0; i<bytes.length*8; i++) {
+			if ((bytes[bytes.length-i/8-1] & (1 << (i%8))) > 0) {
+				bits.set(i);
+			}
+		}
+		return bits;
+	}
+
+	public static byte[] toByteArray(BitSet bits) {
+		byte[] bytes = new byte[bits.length()/8+1];
+		for (int i=0; i<bits.length(); i++) {
+			if (bits.get(i)) {
+				bytes[bytes.length-i/8-1] |= 1 << (i%8);
+			}
+		}
+		return bytes;
+	}
+}
