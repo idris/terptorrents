b73ee68b03e6b043ecc8313d518ed4ceaff0e9e8
samuelbrin <samuelbrin@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-28 06:03:01 +0000

    done coding single file case

--- trunk/terptorrents/src/metainfo/MetaFile.java
+++ trunk/terptorrents/src/metainfo/MetaFile.java
@@ -9,47 +9,70 @@ import terptorrents.models.Piece;
 public class MetaFile {
 
 	private String announce;
-	//private List<String> announceList;
+	// private List<String> announceList;
 	private Date creationDate;
 	private String comment;
 	private String createdBy;
-	//private String encoding;
+	// private String encoding;
 	private Long pieceLength;
 	private List<Piece> pieces;
 	private List<String> filenames;
-	private Map<String,Long> fileLengths;
-	private Map<Integer,byte[]> SHAHashes;
+	private Map<String, Long> fileLengths;
+	private Map<Integer, byte[]> SHAHashes;
+
+	public MetaFile(String announce, Date creationDate, String comment,
+			String createdBy, Long pieceLength, List<String> filenames,
+			Map<String, Long> fileLengths, Map<Integer, byte[]> hashes) {
+
+		this.announce = announce;
+		this.creationDate = creationDate;
+		this.comment = comment;
+		this.createdBy = createdBy;
+		this.pieceLength = pieceLength;
+		this.filenames = filenames;
+		this.fileLengths = fileLengths;
+		SHAHashes = hashes;
+	}
+
 	public String getAnnounce() {
 		return announce;
 	}
+
 	public Date getCreationDate() {
 		return creationDate;
 	}
+
 	public String getComment() {
 		return comment;
 	}
+
 	public String getCreatedBy() {
 		return createdBy;
 	}
+
 	public Long getPieceLength() {
 		return pieceLength;
 	}
+
 	public List<Piece> getPieces() {
 		return pieces;
 	}
+
 	public List<String> getFilenames() {
 		return filenames;
 	}
-	
-	/* This will be a LinkedHashMap under the hood, so insertion order is preserved by the iterator */
+
+	/*
+	 * This will be a LinkedHashMap under the hood, so insertion order is
+	 * preserved by the iterator
+	 */
 	public Map<String, Long> getFileLengths() {
 		return fileLengths;
-	} 
-	
+	}
+
 	/* Returns a map from piece numbers to SHA Hash values */
-	public Map<Integer,byte[]> getSHAHashes(){
+	public Map<Integer, byte[]> getSHAHashes() {
 		return SHAHashes;
 	}
-	
-	
+
 }
