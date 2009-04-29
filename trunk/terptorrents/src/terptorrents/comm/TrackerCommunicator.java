821860d133dcd5cd625b5e4c6bbd630c9eb2f9e2
idrism <idrism@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-29 01:23:54 +0000

    fix stuff up

--- trunk/terptorrents/src/terptorrents/comm/TrackerCommunicator.java
+++ trunk/terptorrents/src/terptorrents/comm/TrackerCommunicator.java
@@ -2,35 +2,77 @@ package terptorrents.comm;
 
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.net.InetAddress;
 import java.net.URL;
 import java.net.URLConnection;
 import java.nio.CharBuffer;
 
+import terptorrents.Stats;
 import terptorrents.tracker.TrackerRequest;
 import terptorrents.tracker.TrackerResponse;
 
 public class TrackerCommunicator implements Runnable {
-	private static final long REQUEST_INTERVAL = 1000*60*30; // thirty minutes
+	private static final String EVENT_STARTED = "started";
+	private static final String EVENT_STOPPED = "stopped";
+	private static final String EVENT_COMPLETED = "completed";
+
+	private long requestInterval = 1000*60*30; // thirty minutes
+	private String trackerId = null;
+	private final String peerId;
+	private final int port;
+	private final String infoHash;
+	private final Stats stats = Stats.getInstance();
+	private final boolean compact = true;
+	private boolean completed = false;
+	private boolean stopped = false;
+
+	public TrackerCommunicator() throws IOException {
+		// TODO: use real values
+		peerId = "my peer id";
+		port = 1234;
+		infoHash = "some info hash here";
+		pingTracker(EVENT_STARTED);
+	}
 
 	public void run() {
-		try {
-			pingTracker();
-		} catch(IOException ex) {
-			// oh well... maybe next time.
+		while(!completed) {
+			try {
+				Thread.sleep(requestInterval);
+			} catch(InterruptedException ex) {
+				// keep going
+			}
+
+			try {
+				pingTracker(null);
+			} catch(IOException ex) {
+				// oh well... maybe next time.
+			}
 		}
 
-		try {
-			Thread.sleep(REQUEST_INTERVAL);
-		} catch(InterruptedException ex) {
-			// keep going
+		while(!stopped) {
+			try {
+				pingTracker(EVENT_COMPLETED);
+			} catch(IOException ex) {
+				// oh well... maybe next time.
+			}
+
+			try {
+				Thread.sleep(requestInterval);
+			} catch(InterruptedException ex) {
+				// keep going
+			}
 		}
 	}
 
-	public void pingTracker() throws IOException {
+	public void stop() throws IOException {
+		stopped = true;
+		pingTracker(EVENT_STOPPED);
+	}
+
+	private void pingTracker(String event) throws IOException {
 		String trackerURL = "http://blahblahblah";
 
-		TrackerRequest request = new TrackerRequest();
-		trackerURL += request.toQueryString();
+		trackerURL += generateQueryString(event);
 
 		URL url = new URL(trackerURL);
 		URLConnection conn = url.openConnection();
@@ -39,10 +81,32 @@ public class TrackerCommunicator implements Runnable {
 		CharBuffer charBuf = CharBuffer.wrap(buf);
 		while(reader.read(charBuf) > 0);
 
-		String responseText = buf.toString();
+		handleResponse(buf.toString());
+	}
 
-		TrackerResponse response = new TrackerResponse(responseText);
+	private String generateQueryString(String event) {
+		int left = 555; // TODO: get number of bytes remaining from IO
 
-		// do something with the response
+		String str = "info_hash=" + infoHash +
+		"peer_id" + peerId +
+		"port" + port +
+		"uploaded" + stats.uploaded.get() +
+		"downloaded" + stats.downloaded.get() +
+		"left" + left +
+		"compact" + (compact ? "1" : "0") +
+//		"no_peer_id" + no_peer_id +
+		"event" + event;
+//		"ip" + ip +
+//		"numwant" + numwant +
+//		"key" + key +
+		if(trackerId != null) {
+			str += "trackerid" + trackerId;
+		}
+
+		return str;
+	}
+
+	private void handleResponse(String response) {
+		// bdecode and do stuff here.
 	}
 }
