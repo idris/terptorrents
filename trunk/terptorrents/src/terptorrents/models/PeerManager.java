2b72ad891ada6b9ea10b39682659003c0fa7dd22
idrism <idrism@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-04-30 20:12:36 +0000

    addPeers

--- trunk/terptorrents/src/terptorrents/models/PeerManager.java
+++ trunk/terptorrents/src/terptorrents/models/PeerManager.java
@@ -4,17 +4,27 @@
 package terptorrents.models;
 
 import java.util.List;
+import java.util.Vector;
 
 /**
  * @author idris
  *
  */
 public class PeerManager {
+	private static final PeerManager singleton = new PeerManager();
+	private List<Peer> peers = new Vector<Peer>();
+
+	private PeerManager() {}
+
 	public static PeerManager getInstance() {
-		return null;
+		return singleton;
 	}
 
 	public List<Peer> getRandomUnconnectedPeers(int max) {
 		return null;
+	}
+
+	public void addPeers(List<Peer> newPeers) {
+		peers.addAll(newPeers);
 	}
 }
