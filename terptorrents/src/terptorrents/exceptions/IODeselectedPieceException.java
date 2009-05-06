3951280d4bc9d80e194c451f280ff4a5f3420093
Jon Li <Jon Li@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-05-06 17:59:48 +0000

    fix warnings

--- terptorrents/src/terptorrents/exceptions/IODeselectedPieceException.java
+++ terptorrents/src/terptorrents/exceptions/IODeselectedPieceException.java
@@ -1,6 +1,11 @@
 package terptorrents.exceptions;
 
 public class IODeselectedPieceException extends Exception {
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 8087485034662929354L;
+
 	public IODeselectedPieceException(String msg) {
 		super(msg);
 	}
