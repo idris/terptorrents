25254a148b623117fb81b2bd3354715cb5ef97df
sshlyk <sshlyk@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-05-06 15:42:44 +0000

    Disconnect from the peers if we do not receive requested pieces after some period of time

--- trunk/terptorrents/src/terptorrents/models/ChockingAlgorithm.java
+++ trunk/terptorrents/src/terptorrents/models/ChockingAlgorithm.java
@@ -23,30 +23,110 @@ import terptorrents.io.IO;
 public class ChockingAlgorithm implements Runnable {
 	static int countOptimistic = 0;
 
+	
+	//TODO empty RequestedBlock only after certain time period 
 	public void run() {
+		HashSet<BlockRange> RequestedBlock = new HashSet<BlockRange>();
+		HashSet<BlockRange> copyOfRequestedBlocks = new HashSet<BlockRange>();
+		long timeStamp = System.currentTimeMillis();
+		long curTimeSnapShot;
+		int numOfRetries = 0;
+		Vector<PeerConnection> peersWeSendRequestsTo = new Vector<PeerConnection>();
+		Vector<PeerConnection> copyOfpeersWeSendRequestsTo = new Vector<PeerConnection>();
+		ClientMode clientMode;
+		
 		while(true){
-			HashSet<BlockRange> RequestedBlock = new HashSet<BlockRange>();
-			/*send request message*/
-			for(PeerConnection peerCon: ConnectionPool.
-					getInstance().getNonChokingAndInsterested()){
-				try {
-					ArrayList<BlockRange> blockRanges = PieceManager.getInstance().
-					getBlockRangeToRequest(peerCon.getPeer(), RequestedBlock);
-					for(BlockRange br : blockRanges){
-						RequestedBlock.add(br);
-						peerCon.sendMessage(new RequestMessage(
-								br.getPieceIndex(), br.getBegin(), 
-								br.getLength()));
+			/* detect our client mode */
+			if (IO.getInstance().isComplete())
+				clientMode = ClientMode.SEEDING;
+			else clientMode = ClientMode.LEECHING;
+
+			/* SEND REQUESTS IN LEECHING MODE */
+			if (clientMode == ClientMode.LEECHING) {
+				/*
+				 * begin. Sergey
+				 * reset RequestedBlock set only after some period
+				 * of time to avoid constant retransmission of requests to the
+				 * same peers
+				 */
+				curTimeSnapShot = System.currentTimeMillis();
+				if (curTimeSnapShot - timeStamp > Main.TIME_BETWEEN_RETRANSMITION_OF_UNREPLIED_REQUEST_MESSAGES) {
+					/*
+					 * rotate peers if no piece message received after certain
+					 * number of duplicate request of some block to the same
+					 * peers
+					 */
+					if (copyOfRequestedBlocks.equals(RequestedBlock)
+							&& !copyOfRequestedBlocks.isEmpty()) {
+						numOfRetries++;
+						/*
+						 * if we did not hear reply from the same peers after
+						 * some number of retries. Give up.
+						 */
+						if (numOfRetries == Main.MAX_NUM_RETRANSMISSIONs_OF_REQUEST_MESSAGES) {
+							Main.dprint("Same set of request have been sent "
+									+ numOfRetries + " times");
+							if (copyOfpeersWeSendRequestsTo
+									.equals(peersWeSendRequestsTo)
+									&& !copyOfpeersWeSendRequestsTo.isEmpty()) {
+								Main.dprint("Rotating peers...");
+								for (PeerConnection pc : peersWeSendRequestsTo) {
+									// TODO set interested or it is better to
+									// Disconnect?
+									//pc.setInterested(false);
+									//pc.setChoked(true);
+									ConnectionPool.getInstance().removeConnection(pc);
+									Main.dprint("No blocks received from " + pc.toString() 
+											+ " after " + numOfRetries 
+											+ " retries. DISCONNECTING");
+								}
+							} else
+								copyOfpeersWeSendRequestsTo = peersWeSendRequestsTo;
+							numOfRetries = 0;
+						}
+					} else
+						copyOfRequestedBlocks = RequestedBlock;
+					RequestedBlock = new HashSet<BlockRange>();
+					timeStamp = curTimeSnapShot;
+				}
+				/* end. Sergey */
+
+				/* send request message */
+				peersWeSendRequestsTo = ConnectionPool.getInstance()
+						.getNonChokingAndInsterested();
+				/* Sergey
+				 * Changed for-loop so we remove peer immediately if we
+				 * can not request from this peer. Otherwise loop will cause
+				 * multilple request to such peers
+				 */
+				PeerConnection peerCon;
+				for (int i = 0 ; i < peersWeSendRequestsTo.size() ; i++) {
+					peerCon = peersWeSendRequestsTo.get(i);
+					try {
+						ArrayList<BlockRange> blockRanges = PieceManager
+								.getInstance().getBlockRangeToRequest(
+										peerCon.getPeer(), RequestedBlock);
+						for (BlockRange br : blockRanges) {
+							RequestedBlock.add(br);
+							peerCon.sendMessage(new RequestMessage(br
+									.getPieceIndex(), br.getBegin(), br
+									.getLength()));
+						}
+					} catch (TerptorrentsModelsCanNotRequstFromThisPeer e) {
+						if (Main.DEBUG)
+							//TODO We have to do something about it
+							// Remove peer? set unInterested
+							System.err
+									.println("Can not request from this peer EXCEPTION IS CAUGHT: "
+											+ peerCon.getPeer().toString());
+						Main.dprint("Removing peer from request list");
+						peersWeSendRequestsTo.remove(peerCon);
 					}
-				} catch (TerptorrentsModelsCanNotRequstFromThisPeer e) {
-					if (Main.DEBUG)
-						System.err.println("Can not request from this peer EXCEPTION IS CAUGHT: " + peerCon.getPeer().toString());
-					//e.printStackTrace();
 				}
 			}
 
 			/*choking algorithm*/
-			if(IO.getInstance().isComplete()){
+			if(clientMode == ClientMode.SEEDING){
 				//seeding mode
 				Vector<PeerConnection> peersToUnchoke = 
 					ConnectionPool.getInstance().getSeedableConnections();
@@ -121,11 +201,11 @@ public class ChockingAlgorithm implements Runnable {
 						// Sergey. Boundary checking 
 						int min = Math.min(uploaderList.size(), Main.NUM_PEERS_TO_UNCHOKE);
 						if (min == 0) break; //no one to unchoke
-						while(uploaderList.subList(0, Main.NUM_PEERS_TO_UNCHOKE).
-								contains(plannedOptimisticUnchokedPeer))
+						while(uploaderList.subList(0, min)
+								.contains(plannedOptimisticUnchokedPeer))
 							plannedOptimisticUnchokedPeer = 
-								ConnectionPool.getInstance().
-								getPlannedOptimisticUnchokedPeerConnection();
+								ConnectionPool.getInstance()
+								.getPlannedOptimisticUnchokedPeerConnection();
 
 						plannedOptimisticUnchokedPeer.
 						sendMessage(new UnchokeMessage());
@@ -140,4 +220,6 @@ public class ChockingAlgorithm implements Runnable {
 			}
 		}
 	}
+	
+	private enum ClientMode {LEECHING, SEEDING}
 }
