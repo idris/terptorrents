fdacf5e1f11d23fcae4a6b9916e0a895df294cf4
Jon Li <Jon Li@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-05-06 17:59:48 +0000

    fix warnings

--- trunk/terptorrents/src/terptorrents/exceptions/TerptorrentsIONoSuchPieceException.java
+++ trunk/terptorrents/src/terptorrents/exceptions/TerptorrentsIONoSuchPieceException.java
@@ -2,6 +2,11 @@ package terptorrents.exceptions;
 
 public class TerptorrentsIONoSuchPieceException extends Exception{
 
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = -7663503228352421985L;
+
 	public TerptorrentsIONoSuchPieceException(String string) {
 		super(string);
 	}
