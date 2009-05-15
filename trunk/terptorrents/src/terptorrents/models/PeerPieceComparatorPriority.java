d9343f4218ed0d456d62d8b199507f2531befbf5
Jon Li <Jon Li@737fa17e-2b64-11de-bfb9-e98281094dd5>
2009-05-15 01:15:36 +0000

    stupid bug fix for priority comparator

--- trunk/terptorrents/src/terptorrents/models/PeerPieceComparatorPriority.java
+++ trunk/terptorrents/src/terptorrents/models/PeerPieceComparatorPriority.java
@@ -7,7 +7,7 @@ public class PeerPieceComparatorPriority implements Comparator<PeerPiece> {
 	public int compare(PeerPiece arg0, PeerPiece arg1) {
 		if(arg0.getPriority() == arg1.getPriority())
 			return 0;
-		else if (arg0.getPriority() < arg1.getPriority())
+		else if (arg0.getPriority() > arg1.getPriority())
 			return -1;
 		else
 			return 1;
